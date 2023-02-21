2b5ae18ba81f968cfe9555df8156651881e4f510
DESKTOP-CGH48O5\laure <laurenkim002@naver.com>
2023-02-21 18:42:32 +0900

    [김혜민] - lifecycle test1, test2 수정

--- src/main/java/com/malloc/spring/test/lifecycle/Test01Controller.java
+++ src/main/java/com/malloc/spring/test/lifecycle/Test01Controller.java
@@ -8,25 +8,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/lifecycle/test01")
 public class Test01Controller {
 	
 	@ResponseBody
-	@RequestMapping("/lifecycle/test01/1")
-	public String testProject() {
+	@RequestMapping("/1")
+	public String stringResponse() {
 		
-		return "<h1>테스트 프로젝트 완성</h1>해당 프로젝트를 통해서 문제 풀이를 진행합니다.";
+		return "<h1>테스트 프로젝트 완성</h1><h4>해당 프로젝트를 통해서 문제 풀이를 진행합니다.</h4>";
 	}
 	
 	
 	@ResponseBody
-	@RequestMapping("/lifecycle/test01/2")
-	public Map<String, Integer> mapPrint() {
+	@RequestMapping("/2")
+	public Map<String, Integer> mapResponse() {
 		
 		Map<String, Integer> grade = new HashMap<>();
 		
 		grade.put("국어", 80);
-		grade.put("영어", 90);
-		grade.put("수학", 85);
+		grade.put("수학", 90);
+		grade.put("영어", 85);
 		
 		return grade;
 	}
--- src/main/java/com/malloc/spring/test/lifecycle/Test02Controller.java
+++ src/main/java/com/malloc/spring/test/lifecycle/Test02Controller.java
@@ -5,25 +5,29 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.malloc.spring.test.lifecycle.model.Bulletin;
 
 @RestController
-@RequestMapping("/lifecycle/test01")
+@RequestMapping("/lifecycle/test02")
 public class Test02Controller {
 	
-	@RequestMapping("/3")
-	public List<Map<String, Object>> jsonPrintMist() {
-		List<Map<String, Object>> movieList = new ArrayList();
+	@RequestMapping("/1")
+	public List<Map<String, Object>> jsonPrintMap() {
+		List<Map<String, Object>> movieList = new ArrayList<>();
 		
-		Map<String, Object> movie1 = new HashMap();
+		Map<String, Object> movie1 = new HashMap<>();
 		movie1.put("rate", 15);
 		movie1.put("director", "봉준호");
 		movie1.put("time", 131);
 		movie1.put("title", "기생충");
 		movieList.add(movie1);
+		//이미 데이터의 주소는 list에 들어갔기 때문에 movie2처럼 새로 안만들고 그냥
+		//movie1 = new HashMap()으로 재활용 해도 됨.
 		
 		Map<String, Object> movie2 = new HashMap();
 		movie2.put("rate", 0);
@@ -42,7 +46,7 @@ public class Test02Controller {
 		return movieList;
 	}
 	
-	@RequestMapping("/4")
+	@RequestMapping("/2")
 	public List<Bulletin> jsonPrintClass() {
 		
 		List<Bulletin> bulletinList = new ArrayList();
@@ -67,4 +71,20 @@ public class Test02Controller {
 		
 		return bulletinList;
 	}
+	
+	
+	@RequestMapping("/3")
+	public ResponseEntity<Bulletin> entityResponse() {
+		Bulletin bulletin = new Bulletin();
+		
+		bulletin.setTitle("안녕하세요. 가입인사 드립니다.");
+		bulletin.setUser("hagulu");
+		bulletin.setContent("안녕하세요. 가입했어요. 잘 부탁드립니다. ");
+		
+		ResponseEntity<Bulletin> entity = new ResponseEntity(bulletin, HttpStatus.INTERNAL_SERVER_ERROR);
+		
+		return entity;
+	}
+	
+	
 }
