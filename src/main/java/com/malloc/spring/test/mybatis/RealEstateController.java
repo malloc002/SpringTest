09d8582fe01eba8a2a915e869b143b3ce641c5ed
DESKTOP-CGH48O5\laure <laurenkim002@naver.com>
2023-02-24 18:00:20 +0900

    [김혜민] - mybatis 실습 1문제 test2

--- src/main/java/com/malloc/spring/test/mybatis/RealEstateController.java
+++ src/main/java/com/malloc/spring/test/mybatis/RealEstateController.java
@@ -12,13 +12,15 @@ import com.malloc.spring.test.mybatis.bo.RealEstateBO;
 import com.malloc.spring.test.mybatis.model.RealEstate;
 
 @Controller
+@RequestMapping("/mybatis")
 public class RealEstateController {
 	
 	@Autowired
 	private RealEstateBO realEstateBO;
 	
+	//전달받은 
 	@ResponseBody
-	@RequestMapping("/mybatis/test01/1")
+	@RequestMapping("/test01/1")
 	public RealEstate realEstate(@RequestParam("id") int id) {
 		
 		RealEstate realEstate = realEstateBO.getRealEstate(id);
@@ -26,8 +28,9 @@ public class RealEstateController {
 		return realEstate;
 	}
 	
+	//전달받은 월세 금액보다 낮은 매물 정보를 json으로 response 담기
 	@ResponseBody
-	@RequestMapping("/mybatis/test01/2")
+	@RequestMapping("/test01/2")
 	public List<RealEstate> rentPrice(@RequestParam("rp") int rp) {
 		
 		List<RealEstate> rentPriceList = realEstateBO.getRentPrice(rp);
@@ -37,11 +40,31 @@ public class RealEstateController {
 	}
 	
 	@ResponseBody
-	@RequestMapping("/mybatis/test01/3")
+	@RequestMapping("/test01/3")
 	public List<RealEstate> areaPrice(@RequestParam("area") int area, @RequestParam("price") int price) {
 		
 		List<RealEstate> areaPriceList = realEstateBO.getAreaPrice(area, price);
 		
 		return areaPriceList;
+	}
+	
+	@ResponseBody
+	@RequestMapping("/test02")
+	public String addRealEstate(@RequestParam("realtorId") int realtorId) {
+		 
+//		RealEstate realEstate = new RealEstate();
+//		realEstate.setRealtorId(3);
+//		realEstate.setAddress("푸르지용 리버 303동 1104호");
+//		realEstate.setArea(89);
+//		realEstate.setType("매매");
+//		realEstate.setPrice(100000);
+//		
+//		int count = realEstateBO.addRealEstateByObject(realEstate);
+		
+		//realtorId, "썅떼빌리버 오피스텔 814호", 45, "월세", 100000, 120
+		int count = realEstateBO.addRealEstateByField(realtorId, "썅떼빌리버 오피스텔 814호", 45, "월세", 100000, 120);
+		
+		return "실행결과: " + count;
+		
 	}
 }
--- src/main/java/com/malloc/spring/test/mybatis/bo/RealEstateBO.java
+++ src/main/java/com/malloc/spring/test/mybatis/bo/RealEstateBO.java
@@ -14,6 +14,7 @@ public class RealEstateBO {
 	@Autowired
 	private RealEstateDAO realEstateDAO;
 	
+	//전달받은 id와 일치하는 데이터 조회 리턴
 	public RealEstate getRealEstate(int id) {
 		
 		RealEstate realEstate = realEstateDAO.selectRealEstate(id);
@@ -21,6 +22,7 @@ public class RealEstateBO {
 		return realEstate;
 	}
 	
+	//전달받은 월세 금액보다 낮은 매물 정보들 리턴
 	public List<RealEstate> getRentPrice(int rp) {
 		
 		List<RealEstate> rentPriceList = realEstateDAO.selectRentPrice(rp);
@@ -34,5 +36,15 @@ public class RealEstateBO {
 		List<RealEstate> areaPriceList = realEstateDAO.selectAreaPrice(area, price);
 		
 		return areaPriceList;
+	}
+	
+	public int addRealEstateByObject(RealEstate realEstate) {
+		
+		return realEstateDAO.insertRealEstate(realEstate);
+	}
+	
+	public int addRealEstateByField(int realtorId, String address, int area, String type, int price, int rentPrice) {
+		
+		return realEstateDAO.insertRealEstateByField(realtorId, address, area, type, price, rentPrice);
 	}
 }
